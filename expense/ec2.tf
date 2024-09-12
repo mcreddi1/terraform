87629de29d3942e96b34769f434652f079fc782c
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-12 12:50:06 +0530

    terraform

--- expense/ec2.tf
+++ expense/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "expense" {
     count = length(var.instance_names)
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform]
     tags = merge(
         var.common-tags,
         {
