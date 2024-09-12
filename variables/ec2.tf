d3acaf81d9dbe9fce4301882a849516faf5c9dc4
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-12 10:03:34 +0530

    terraform

---
+++ variables/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = var.tags
+    }
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = var.sg_name #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_sshh"
+    }
+}
---
+++ variables/vars.tf
@@ -0,0 +1,42 @@
+variable "ami_id" {
+  type = string
+  default = "ami-09c813fb71547fc4f"
+  description = "this is the ami id for devops practice"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "tags" {
+ default = {
+  Name = backend
+  project = dev
+  component = dev
+  terraform = true
+  }
+}
+
+variable "sg_name" {
+  default = "allow_ssh"
+}
+
+variable "from_port" {
+  default = 22
+}
+
+variable "to_port" {
+  default = 22
+  type = number
+}
+
+variable "protocol" {
+  default = "tcp"
+  type = number
+}
+
+variable "cidr_blocks" {
+  default = ["0.0.0.0/0"]
+  type = list(string)
+}
