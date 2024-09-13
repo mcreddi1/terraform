086ae6d3aa7ba4ffef4062f72697edc11cc8ef1b
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-13 13:25:01 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,27 @@
+variable "instance_names" {
+  type = list(string)
+  default = [ "mysql", "frontend", "backend" ]
+}
+
+variable "sg_name" {
+  default = "allow_sshport"
+}
+
+variable "from_port" {
+  default = 22
+}
+
+variable "to_port" {
+  default = 22
+  type = number
+}
+
+variable "protocol" {
+  default = "tcp"
+  type = string
+}
+
+variable "cidr_blocks" {
+  default = ["0.0.0.0/0"]
+  type = list(string)
+}
