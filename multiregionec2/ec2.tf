abad1a4da76c5ae1c1c74fbdb5388c73601e48ed
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-14 15:29:07 +0530

    terraform

--- multiregionec2/ec2.tf
+++ multiregionec2/ec2.tf
@@ -9,13 +9,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = var.ami_id
+  ami = "ami-0583d8c7a9c35822c"
   instance_type = "t2.micro"
   provider = "aws.us-east-1"
 }
 
 resource "aws_instance" "example2" {
-  ami = var.ami_id
+  ami = "ami-0583d8c7a9c35822c"
   instance_type = "t2.micro"
   provider = "aws.us-west-2"
 }
