bcd0d4881bff9928f8baff9ebbffc7dd6bd2afd9
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-16 17:12:28 +0530

    devops practice

---
+++ provisioners/ec2.tf
@@ -0,0 +1,57 @@
+resource "aws_instance" "expense" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = merge(
+        var.common-tags,
+        {
+            Name = Provisionrer
+        }
+    )
+    # local exec will execute commands we provided on local computer
+    # remote exec will connect to the remote server and install the required packages
+    provisioner "local-exec" {
+    command = "echo ${self.private_ip} > pub_ip.txt"
+    }
+
+    connection {
+        host = self.public_ip
+        user = "ec2-user"
+        password = "DevOps321"
+        type = "ssh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "sudo dnf install nginx -y",
+        "sudo dnf install ansible -y"
+       ]
+    }
+    
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "allow_sshh" #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_sshh"
+    }
+}
---
+++ provisioners/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+  type = list(string)
+  default = [ "mysql", "frontend", "backend" ]
+}
+
+variable "common-tags" {
+  type = map 
+  default = {
+    project = "expense"
+    environment = "dev"
+    terraform = "true"
+  }
+}
+
+variable "zone_id" {
+  default = "Z0286835M1G9H01UW24U"
+}
+
+variable "domain_name" {
+  default = "devops81s.shop"
+}
